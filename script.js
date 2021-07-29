7ca88e302fca64beec1bf05149ad7e658502290a
codaku@321 <rashiksharma4@gmail.com>
2021-07-29 09:40:36 +0545

    expand and collapse added

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 var Expand = document.querySelector('.btn');
+var Collapse = document.querySelector('.btn1');
 var Content = document.querySelector('.card-description');
 
+
+
 Expand.addEventListener('click', function(){
+
 	Content.style.height = "auto";
-	Expand.style.display = "None";
+	Expand.style.display = "None";	
+	Collapse.style.display ="block";
+
+	Collapse.addEventListener('click',function(){
+		Content.style.height = "50px";
+		Collapse.style.display ="None";
+		Expand.style.display = "block";	
+
+
+
+	});
 
 });
+
+
+
